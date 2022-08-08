93f7ab64bf7d712d9b02053f6b26dd7d321fcbd3
Amisha8899 <105807890+Amisha8899@users.noreply.github.com>
2022-08-08 18:32:21 +0530

    Add files via upload

---
+++ SPIDER T1.js
@@ -0,0 +1,63 @@
+function chance(){
+    var p1 = (Math.floor(Math.random()*5)+1)
+    var p2 = (Math.floor(Math.random()*5)+1)
+    var id1 = "p1"+p1
+    var id2 = "p2"+p2
+    var a = document.getElementById(id1).getAttribute("src")
+    var b = document.getElementById(id2).getAttribute("src")
+    var li=[
+        "C:\\Users\\DELL\\Downloads\\IMG1.jpg",
+        "C:\\Users\\DELL\\Downloads\\IMG2.jpg",
+        "C:\\Users\\DELL\\Downloads\\IMG3.jpg",
+        "C:\\Users\\DELL\\Downloads\\IMG4.jpg",
+        "C:\\Users\\DELL\\Downloads\\IMG5.jpg"
+    ]
+    switch (a) {
+        case li[4]:
+            document.getElementById(id1).src=li[0]
+            break;
+        case li[0]:
+            document.getElementById(id1).src=li[1]
+            break;
+        case li[1]:
+            document.getElementById(id1).src=li[2]
+            break;
+        case li[2]:
+            document.getElementById(id1).src=li[3]
+            var c = document.getElementById("p1s").innerHTML
+            var d = parseInt(c)+5
+            document.getElementById("p1s").innerHTML=d
+            break;
+        default:
+            break;
+    }
+    if(document.getElementById("p1s").innerHTML==25){
+        document.getElementById("top").innerHTML ="Congrats!! Player 1 won🚩"
+        document.getElementById("last").style.visibility="visible"
+        document.getElementById("button").remove()
+    }
+    switch (b) {
+        case li[4]:
+            document.getElementById(id2).src=li[0]
+            break;
+        case li[0]:
+            document.getElementById(id2).src=li[1]
+            break;
+        case li[1]:
+            document.getElementById(id2).src=li[2]
+            break;
+        case li[2]:
+            document.getElementById(id2).src=li[3]
+            var p = document.getElementById("p2s").innerHTML
+            var q = parseInt(p)+5
+            document.getElementById("p2s").innerHTML=q
+            break;
+        default:
+            break;
+    }
+    if(document.getElementById("p2s").innerHTML==25){
+        document.getElementById("top").textContent ="Congrats!! Player 2 won🚩"
+        document.getElementById("last").style.visibility="visible"
+        document.getElementById("button").remove()
+    }
+}
